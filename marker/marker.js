0082855395842a3f807fe01be0846383fd0db3af
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2012-08-27 22:00:34 -0300

    Source Formatting

--- marker/marker.js
+++ marker/marker.js
@@ -1,15 +1,12 @@
 function hideAddressBar() {
-  if(!window.location.hash)
-  {
-      if(document.height <= window.outerHeight + 10)
-      {
-      		setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
-      }
-      else
-      {
-          setTimeout( function(){ window.scrollTo(0, 1); }, 0 );
-      }
-  }
+	if(!window.location.hash) {
+		if(document.height <= window.outerHeight + 10) {
+			setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+		}
+		else {
+			setTimeout( function(){ window.scrollTo(0, 1); }, 0 );
+		}
+	}
 }
 
 window.addEventListener("load", hideAddressBar );
