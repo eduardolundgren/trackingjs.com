b682c178cbf1b333e54cdebca3ae4bf477bcd489
Zeno Rocha <zeno.rocha@liferay.com>
2014-07-22 23:14:41 -0300

    New home layout using PureCSS

---
+++ app/scripts/side-menu.js
@@ -0,0 +1,36 @@
+(function() {
+  'use strict';
+
+  var layout = document.getElementById('layout'),
+    menu = document.getElementById('menu'),
+    menuLink = document.getElementById('menuLink');
+
+  function toggleClass(element, className) {
+    var classes = element.className.split(/\s+/),
+      length = classes.length;
+
+    for (var i = 0; i < length; i++) {
+      if (classes[i] === className) {
+        classes.splice(i, 1);
+        break;
+      }
+    }
+
+    // The className is not found
+    if (length === classes.length) {
+      classes.push(className);
+    }
+
+    element.className = classes.join(' ');
+  }
+
+  menuLink.onclick = function(e) {
+    var active = 'active';
+
+    toggleClass(layout, active);
+    toggleClass(menu, active);
+    toggleClass(menuLink, active);
+
+    e.preventDefault();
+  };
+}());
