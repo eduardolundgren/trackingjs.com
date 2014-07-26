be0687a35a739ccd31aaaf2b724a074a0b5f9ba1
Zeno Rocha <zeno.rocha@liferay.com>
2014-07-25 22:05:45 -0300

    Regenerate

---
+++ menu.soy.js
@@ -0,0 +1,46 @@
+// This file was automatically generated from menu.soy. // Please don't edit this file by hand. if (typeof menu == 'undefined') { var menu = {}; } /** * @param {Object.
+<string, *>=} opt_data * @param {(null|undefined)=} opt_ignored * @return {string} * @notypecheck */ menu.side = function(opt_data, opt_ignored) { return '<a href="#menu" id="menuLink" class="menu-link"><span></span></a>
+    <div id="menu">
+        <div class="pure-menu pure-menu-open" id="' + soy.$$escapeHtml(opt_data.cssScope) + '"><a class="pure-menu-heading home-menu-selected index" href="' + soy.$$escapeHtml(opt_data.baseDir) + '">tracking.js</a>
+            <ul>
+                <li class="main-item">Docs</li>
+                <li class="pure-menu-selected docs"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'docs.html#introduction">Introduction</a>
+                </li>
+                <li class="docs"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'docs.html#trackers">Trackers</a>
+                </li>
+                <li class="docs"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'docs.html#utilities">Utilities</a>
+                </li>
+                <li class="docs"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'docs.html#web-components">Web Components</a>
+                </li>
+                <li class="main-item">Examples</li>
+                <li class="pure-menu-selected face_tag_friends"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'examples/face_tag_friends.html">Tag Friends</a>
+                </li>
+                <li class="pure-menu-selected face_fish_tank"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'examples/face_fish_tank.html">Iron Spheres</a>
+                </li>
+                <li class="pure-menu-selected color_hexgl"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'examples/color_hexgl.html">Racing Car</a>
+                </li>
+                <li class="pure-menu-selected color_draw_something"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'examples/color_draw_something.html">Draw Something</a>
+                </li>
+                <li class="pure-menu-selected color_fish_tank"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'examples/color_fish_tank.html">Random Particles</a>
+                </li>
+                <li class="pure-menu-selected menu-item-divided face_hello_world"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'examples/face_hello_world.html">Face (Image)</a>
+                </li>
+                <li class="pure-menu-selected face_camera"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'examples/face_camera.html">Face (Camera)</a>
+                </li>
+                <li class="pure-menu-selected color_hello_world"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'examples/color_hello_world.html">Color (Image)</a>
+                </li>
+                <li class="pure-menu-selected color_camera"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'examples/color_camera.html">Color (Camera)</a>
+                </li>
+                <li class="pure-menu-selected color_video"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'examples/color_video.html">Color (Video)</a>
+                </li>
+                <li class="pure-menu-selected menu-item-divided fast"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'examples/fast.html">Feature Detection</a>
+                </li>
+                <li class="pure-menu-selected fast_camera"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'examples/fast_camera.html">Fast (Camera)</a>
+                </li>
+                <li class="pure-menu-selected brief"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'examples/brief.html">Feature Descriptor</a>
+                </li>
+                <li class="pure-menu-selected brief_camera"><a href="' + soy.$$escapeHtml(opt_data.baseDir) + 'examples/brief_camera.html">Brief (Camera)</a>
+                </li>
+            </ul>
+        </div>
+    </div>'; };
